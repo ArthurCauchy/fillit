d68fe80aa8751052e3054ab580591b36ec226b03
Arthur CAUCHY <arthur.cauchy@free.fr>
2017-12-01 16:54:41 +0100

    init_grid

--- fillit.h
+++ fillit.h
@@ -6,17 +6,20 @@
 /*   By: acauchy <acauchy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/30 10:28:22 by acauchy           #+#    #+#             */
-/*   Updated: 2017/12/01 11:10:20 by cpaquet          ###   ########.fr       */
+/*   Updated: 2017/12/01 16:52:46 by acauchy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FILLIT_H
 # define FILLIT_H
 
+# define GRID_SIDE 13
+# define GRID_SIZE (GRID_SIDE * GRID_SIDE)
+
 typedef struct	s_grid_s
 {
-	unsigned char	*grid;
-	int				gridsize;
+	char			*grid;
+	int				square_size;
 }				t_grid;
 
 typedef struct	s_tetri
--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: acauchy <acauchy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/01 10:22:17 by acauchy           #+#    #+#             */
-/*   Updated: 2017/12/01 15:20:29 by cpaquet          ###   ########.fr       */
+/*   Updated: 2017/12/01 16:52:43 by acauchy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,4 +32,29 @@ void	exit_usage(void)
 {
 	ft_putendl("usage : fillit <file.fillit>");
 	exit(EXIT_SUCCESS);
+}
+
+/*
+** Create a grid and intitialize it with an optimized minimal
+** square size depending on the parameter nb_tetri.
+** The parameter grid_size is the wanted size of the grid.
+** grid_size of 9 creates a 3*3 grid.
+*/
+
+t_grid	*init_grid(int nb_tetri)
+{
+	t_grid grid;
+   
+	if (!(grid = (t_grid*)malloc(sizeof(t_grid))
+				|| !(grid->grid = (char*)malloc(GRID_SIZE))))
+		exit_error();
+	grid->square_size = min_square(nb_tetri);
+	while (i < GRID_SIZE)
+	{
+		if ()
+			//insert BLOCKED char
+		else
+			grid[i == ];
+	}
+	return (grid);
 }
