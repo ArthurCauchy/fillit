9a1682243d187fc8d0bea6d48415ac2f256ae114
Arthur CAUCHY <arthur.cauchy@free.fr>
2017-12-01 10:16:16 +0100

    update samedi 1-12 10:16

--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: acauchy <acauchy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/30 10:28:22 by acauchy           #+#    #+#             */
-/*   Updated: 2017/11/30 17:16:26 by acauchy          ###   ########.fr       */
+/*   Updated: 2017/12/01 10:15:22 by acauchy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,92 +19,32 @@ typedef struct	s_grid_s
 	int				gridsize;
 }				t_grid;
 
+typedef struct	s_tetri
+{
+	char	letter;
+	char	*code;
+}				t_tetri;
+
 typedef struct	s_tetridata
 {
 	char	*filecode;
 	char	*code;
 }				t_tetridata;
 
+/*
+** validate_tetri.c
+*/
+
 char			*validate_tetri(char *buff);
 
-/*const t_tetridata g_tetridatas[] =
-{
-	{
-		.filecode = "#...#...#...#",
-		.code = "BBB"
-	},
-	{
-		.filecode = "#...#...##",
-		.code = "BBD"
-	},
-	{
-		.filecode = "#...###",
-		.code = "BDD"
-	},
-	{
-		.filecode = "##..#...#",
-		.code = "DGBB"
-	},
-	{
-		.filecode = "###.#",
-		.code = "BHDD"
-	},
-	{
-		.filecode = ".#...#..##",
-		.code = "BBG"
-	},
-	{
-		.filecode = "..#.###",
-		.code = "BGG"
-	},
-	{
-		.filecode = "###",
-		.code = "DDD"
-	},
-	{
-		.filecode = "##...#...#",
-		.code = "DBB"
-	},
-	{	
-		.filecode = "###...#",
-		.code = "DDB"
-	},
-	{
-		.filecode = "##...##",
-		.code = "DBD"
-	},
-	{
-		.filecode = ".##.##",
-		.code = "DGBG"
-	},
-	{
-		.filecode = ".#..##..#",
-		.code = "BGB"
-	},
-	{
-		.filecode = "#...##...#",
-		.code = "BDB"
-	},
-	{
-		.filecode = "#...##..#",
-		.code = "BDGB"
-	},
-	{
-		.filecode = ".#..##...#",
-		.code = "BGDB"
-	},
-	{
-		.filecode = ".#..###",
-		.code = "BGDD"
-	},
-	{
-		.filecode = "###..#",
-		.code = "DDGB"
-	},
-	{
-		.filecode = "##..##",
-		.code = "DBG"
-	}
-};*/
+/*
+** import.c
+*/
+
+void			error(void);
+void			lect(int fd, t_tetri *tab_tetri);
+void			usage(void);
+void			import(t_tetri *tab_tetri);
+int				add_tetri(char *id, t_tetri);
 
 #endif
